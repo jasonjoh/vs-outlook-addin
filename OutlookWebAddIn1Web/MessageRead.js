b58864e1177f2d8980c62859c81c4be8cfc431ab
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-08-01 10:28:08 -0400

    End result of quick start
    Ran through the Visual Studio steps from https://docs.microsoft.com/en-us/outlook/add-ins/quick-start?tabs=visual-studio, this is the result.

---
+++ OutlookWebAddIn1Web/MessageRead.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+(function () {
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            loadItemProps(Office.context.mailbox.item);
+        });
+    };
+
+    function loadItemProps(item) {
+        // Get the table body element
+        var tbody = $('.prop-table');
+
+        // Add a row to the table for each message property
+        tbody.append(makeTableRow("Id", item.itemId));
+        tbody.append(makeTableRow("Subject", item.subject));
+        tbody.append(makeTableRow("Message Id", item.internetMessageId));
+        tbody.append(makeTableRow("From", item.from.displayName + " &lt;" +
+            item.from.emailAddress + "&gt;"));
+    }
+
+    function makeTableRow(name, value) {
+        return $("<tr><td><strong>" + name +
+            "</strong></td><td class=\"prop-val\"><code>" +
+            value + "</code></td></tr>");
+    }
+
+})();
